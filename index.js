c986e9258187ba6cb0f7180c2a399696c9afa06a
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-26 05:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,16 +27,40 @@ function dec(n){
 }
 
 function makeInt(n){
-  parseInt('n');
+ if (parseInt('n') = 'n');
   return n;
-}
-
-function makeInt(n){
-  parseInt('n', 10);
+} else if  (parseInt('n', 10) = 0);
   return n;
-} 
+} else { (parseInt('n') = true);
+  return n;
+  
 
 function preserveDecimal(n){
   parseFloat('n');
   return n;
 }
+
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
+
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
