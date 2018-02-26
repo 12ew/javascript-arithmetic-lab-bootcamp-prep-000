edf0dfd3ec52e0a41d0e2067a3f32ac5218f2e84
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-26 06:10:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ function dec(n){
 }
 
 function makeInt(n) {
- parseInt('n', 10);
+ parseInt(n, 10);
   return n;
 } 
 
 
 function preserveDecimal(n) {
-  parseFloat('n');
+  parseFloat(n);
   return n;
 } 
